949bd29b1d2f5531b5f7d872fca60a996e0e1b4f
AAfzal5 <asimafzala05@gmail.com>
2023-01-05 06:52:07 +0530

    fixed some bugs.

--- script.js
+++ script.js
@@ -11,14 +11,14 @@ const swapBtn = document.querySelector('.swap');
 //Function to change the Number from Binary base the given base.
 const fromBinary = (toNumberBase, number) => {
     switch(toNumberBase){
-        case 'decimal':
+        case "decimal":
             return parseInt(number,2).toString(10);
 
-        case 'octal':
+        case "octal":
             return parseInt(number,2).toString(8);
 
-        case 'hexadecimal':
-            return parseInt(number,2).toString(6);
+        case "hexadecimal":
+            return parseInt(number,2).toString(16);
 
         default:
             return number;
@@ -28,14 +28,14 @@ const fromBinary = (toNumberBase, number) => {
 //Function to change the Number from HexaDecimal base the given base.
 const fromHexadecimal = (toNumberBase, number) => {
     switch(toNumberBase){
-        case 'decimal':
-            return parseInt(number,6).toString(10);
+        case "decimal":
+            return parseInt(number,16).toString(10);
 
-        case 'octal':
-            return parseInt(number,6).toString(8);
+        case "octal":
+            return parseInt(number,16).toString(8);
 
-        case 'binary':
-            return parseInt(number,6).toString(2);
+        case "binary":
+            return parseInt(number,16).toString(2);
             
         default:
             return number;
@@ -45,14 +45,14 @@ const fromHexadecimal = (toNumberBase, number) => {
 //Function to change the Number from Octal base the given base.
 const fromOctal = (toNumberBase, number) => {
     switch(toNumberBase){
-        case 'decimal':
+        case "decimal":
             return parseInt(number,8).toString(10);
 
-        case 'binary':
+        case "binary":
             return parseInt(number,8).toString(2);
 
-        case 'hexadecimal':
-            return parseInt(number,8).toString(6);
+        case "hexadecimal":
+            return parseInt(number,8).toString(16);
             
         default:
             return number;
@@ -62,14 +62,14 @@ const fromOctal = (toNumberBase, number) => {
 //Function to change the Number from Decimal base the given base.
 const fromDecimal = (toNumberBase, number) => {
     switch(toNumberBase){
-        case 'binary':
+        case "binary":
             return parseInt(number,10).toString(2);
 
-        case 'octal':
+        case "octal":
             return parseInt(number,10).toString(8);
 
-        case 'hexadecimal':
-            return parseInt(number,10).toString(6);
+        case "hexadecimal":
+            return parseInt(number,10).toString(16);
             
         default:
             return number;
@@ -82,25 +82,26 @@ const convert = () => {
     let fromNumberBase = fromElmnt.value;
     let toNumberBase = toElmnt.value;
     let number = fromTextAreaElmnt.value;
+    let convertedValue = 0;
     
-    if(fromNumberBase === 'binary'){
+    if(fromNumberBase === "binary"){
 
-        let convertedValue = fromBinary(toNumberBase, number);
+        convertedValue = fromBinary(toNumberBase, number);
         toTextAreaElmnt.innerHTML = `${convertedValue}`;
 
-    }else if(fromNumberBase === 'hexadecimal'){
+    }else if(fromNumberBase === "hexadecimal"){
 
-        let convertedValue = fromHexadecimal(toNumberBase, number);
+        convertedValue = fromHexadecimal(toNumberBase, number);
         toTextAreaElmnt.innerHTML = `${convertedValue}`;
 
-    }else if(fromNumberBase === 'octal'){
+    }else if(fromNumberBase === "octal"){
 
-        let convertedValue = fromOctal(toNumberBase, number);
+        convertedValue = fromOctal(toNumberBase, number);
         toTextAreaElmnt.innerHTML = `${convertedValue}`;
 
-    }else if(fromNumberBase === 'decimal'){
+    }else if(fromNumberBase === "decimal"){
 
-        let convertedValue = fromDecimal(toNumberBase, number);
+        convertedValue = fromDecimal(toNumberBase, number);
         toTextAreaElmnt.innerHTML = `${convertedValue}`;
 
     }
@@ -108,8 +109,8 @@ const convert = () => {
 
 const swap = () => {
 
-    let fromNumberBase = parseInt(fromElmnt.value);
-    let toNumberBase = parseInt(toElmnt.value);
+    let fromNumberBase = fromElmnt.value;
+    let toNumberBase = toElmnt.value;
     let temp;
     temp = fromNumberBase;
     fromNumberBase = toNumberBase;
